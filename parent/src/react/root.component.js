46d464bdf79443e486d3f91fa404b5cdf6dda24e
Karthik Arumugam <chivalrykarthik@gmail.com>
2019-12-29 19:08:04 +0530

    Added menu

--- parent/src/react/root.component.js
+++ parent/src/react/root.component.js
@@ -1,5 +1,28 @@
 import React from "react"
+import {BrowserRouter as Router, Route,Link} from 'react-router-dom';
+import './style.css';
 
-const App = () => <h1>Hello from React</h1>
+const Count = props =>{
+    return <div>CountApp</div>
+}
+const Nav = props => {
+    const navigateTo = url => window.history.pushState(null, null, url)
+    return (
+        <ul>
+            <li><a onClick = {()=>navigateTo('/')} >Home</a></li>
+            <li><a onClick = {()=>navigateTo('/app2')} >App2</a></li>
+        </ul>
+    )
+}
+const App = ()=>{
+    return(
+        <Router>
+            <Nav />
+            <Route exact path = '/' component = {Count} />
+
+            
+        </Router>
+    )
+}
 
 export default App
